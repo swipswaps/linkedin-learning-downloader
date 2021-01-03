2e32d7096afc4f9ee1378b786837d6d4ebb33a37
Volodymyr Yepishev <i.m.bwca@gmail.com>
2021-01-03 14:37:35 +0300

    dealt with error 429 + listing failed video downloads

--- src/load-video-download-options/load-video-download-options.function.ts
+++ src/load-video-download-options/load-video-download-options.function.ts
@@ -1,4 +1,4 @@
-import { VideosList } from '../shared';
+import { messageService, Video, VideosList } from '../shared';
 import { loadVideo } from './load-video.function';
 import { DownloadableVideo } from '../shared';
 import { authService } from '../auth/';
@@ -8,5 +8,27 @@ export async function loadVideoDownloadOptions(videosList: VideosList): Promise<
   const q = 'slugs';
   const headers = await authService.getAuthHeaders();
 
-  return Promise.all(videosList.videos.map(async (i) => loadVideo(i, { parentSlug, q, slug: i.slug }, headers)));
+  /** Basically a crutch against 429 error which prevents too many connections */
+  const msTimeout = 40000;
+  const step = 59;
+  let currentIndex = 0;
+  let videosSlice: Video[];
+  const downloadableVideos: DownloadableVideo[] = [];
+  do {
+    videosSlice = videosList.videos.slice(currentIndex, currentIndex + step);
+    currentIndex += step;
+    const downloadableBatch = await Promise.all(videosSlice.map(async (i) => loadVideo(i, { parentSlug, q, slug: i.slug }, headers)));
+    if (videosSlice.length === step) {
+      await new Promise((resolve) => {
+        messageService.out({
+          text: `\nSleeping for ${msTimeout / 1000} seconds to prevent 429 server error\n`,
+          type: 'info'
+        });
+        setTimeout(() => resolve(true), msTimeout);
+      });
+    }
+    downloadableVideos.push(...downloadableBatch);
+  } while (videosSlice.length);
+
+  return downloadableVideos;
 }
